e6dce5508c42dba7b7ca9f008c4043c98cc84aec
erickrod54 <erickrod54@gmail.com>
2018-03-26 20:56:53 -0500

    arreglo de la tipografia en el titulo

--- views/site/index.php
+++ views/site/index.php
@@ -7,7 +7,7 @@ $this->title = 'My Yii Application';
 <div class="site-index">
 
     <div class="jumbotron">
-        <h1>Congratulations! :) :) si conecta netbeans</h1>
+        <h1>Congratulations!! se conecta con netbeans :) pero y el branch</h1>
 
         <p class="lead">You have successfully created your Yii-powered application.</p>
 
